4d8e939e86e2614aa8781fd46b074326f4e1a8bc
mena5800 <mr.safwat55@gmail.com>
2024-01-02 17:46:21 +0200

    add all files of project and solve task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""0. Gather data from an API"""
+import requests
+from sys import argv
+import json
+
+
+if __name__ == "__main__":
+    id = argv[1]
+    url_employee = 'https://jsonplaceholder.typicode.com/users?id={}'.format(
+        id)
+    url_tasks = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+        id)
+    # .jons() convert json object to dict
+    response_employee = requests.get(url_employee).json()
+    name = response_employee[0]["name"]
+
+    response_tasks = requests.get(url_tasks).json()
+    completed_tasks = []
+
+    for task in response_tasks:
+        if task["completed"] is True:
+            completed_tasks.append(task["title"])
+
+    print("Employee {} is done with tasks({}/{}):".format(name,
+          len(completed_tasks), len(response_tasks)))
+    for task in completed_tasks:
+        print("\t {}".format(task))
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""0. Gather data from an API"""
+
+
+
+if __name__ == "__main__":
+  pass
