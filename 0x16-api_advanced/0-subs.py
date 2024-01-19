ec5158019f3ed7aec874b3327378156055099b35
mena5800 <mr.safwat55@gmail.com>
2024-01-19 03:10:38 +0200

    solve first task in api advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""this module contain function to fetch data from reddit api"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit. If an invalid
+    subreddit is given, the function should return 0.
+
+
+    Arguments:
+      subreddit : string contain the name of redit topic
+
+    Return:
+      n : the number of subscribers (not active users, total subscribers)
+      for a given subreddit.
+    """
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'MyRedditBot/1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()["data"]["subscribers"]
+
+    return 0
